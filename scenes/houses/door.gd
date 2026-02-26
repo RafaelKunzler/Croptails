b085ff685d0f2cace2eba1e06bdfdac98879c1cc
Rafael Rodrigues <111392060+RafaelKunzler@users.noreply.github.com>
2026-02-26 10:46:12 -0300

    add houses with interactable doors

---
+++ scenes/houses/door.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var interacteble_component: InteractableComponent = $InteractebleComponent
+
+func _ready() -> void:
+	interacteble_component.interactable_activated.connect(on_interactable_activated)
+	interacteble_component.interactable_deactivated.connect(on_interactable_deactivated)
+	collision_layer = 1
+	
+func on_interactable_activated() -> void:
+	animated_sprite_2d.play("open_door")
+	collision_layer = 2
+	
+func on_interactable_deactivated() -> void:
+	animated_sprite_2d.play("close_door")
+	collision_layer = 1
